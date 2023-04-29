1b5bb7f5e4411158155c99eb53ce258ba918d420
unknown <vittorbeatz@gmail.com>
2023-04-29 17:31:08 -0300

    add projects

--- js/apps.js
+++ js/apps.js
@@ -6,6 +6,9 @@ const checkbox = document.querySelector("input[name=theme");
 
 
 //pego as cores iniciais e as do black theme
+
+/*Basicamente aqui eu pego todo o estilo computado do html
+e depois eu seleciono o valor tal (--bg-color)*/
 const initialColors = {
     bg: window.getComputedStyle(html).getPropertyValue('--bg-color'),
     preto: window.getComputedStyle(html).getPropertyValue('--preto'),
@@ -20,12 +23,13 @@ const darkMode = {
     rosa: "#B34FFF"
 } 
 
-
+//aqui eu adc o '--' antes da key (bg, preto, roxo, rosa)
 const transformKey = (key) => "--" + key
 
 
 //funcao para mudar de cores
 const changeColors = (colors) => {
+    //pega as keys do obj colors e mapeio ela no css, trocando as cores
     Object.keys(colors).map(key => 
         html.style.setProperty(transformKey(key), colors[key])
     )
